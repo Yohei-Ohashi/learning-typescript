c91e3773f258e6fd554f594520eed6dd3e77229a
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 22:54:05 +0900

    21. ユーザー名とパスワードが合っているかを確認しよう - 論理演算子

---
+++ js/login.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const username = "taniguchi";
+const password = "12345";
+if (username === "taniguchi" && password === "12345") {
+    console.log("ログインしました");
+}
+else {
+    console.log("ログインに失敗しました");
+}
+//# sourceMappingURL=login.js.map
