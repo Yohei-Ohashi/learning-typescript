e7014a0256a7ea49ce62eef26a3da7ead152824f
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 16:25:11 +0900

    8. 足し算してみよう - 算術演算子

---
+++ sum.js
@@ -0,0 +1,8 @@
+console.log(1 + 1);
+console.log(10 - 2);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+console.log(Math.pow(10, 2));
+console.log((1 + 2) * 5);
+console.log(1.9 - 0.1);
