d6055729843a0c759ff58910c6f28cac107cccbe
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 19:39:37 +0900

    14. 型推論とany型を理解しよう

---
+++ js/type.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let tax = 1.1;
+tax = "1.5%";
+console.log(500 * tax);
+//# sourceMappingURL=type.js.map
---
+++ type.ts
@@ -0,0 +1,4 @@
+let tax: any = 1.1;
+
+tax = "1.5%";
+console.log(500 * tax);
