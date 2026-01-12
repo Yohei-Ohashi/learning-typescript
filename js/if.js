394d5853cf6b56a657668229de2204aad261fe19
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 22:34:26 +0900

    19. 所持金が足りるか確認しよう - if

---
+++ js/if.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mymoney = 800; // 所持金が1000円
+if (mymoney >= 800) {
+    console.log("800円の商品は買えます");
+}
+else {
+    console.log("お金が足りません");
+}
+//# sourceMappingURL=if.js.map
