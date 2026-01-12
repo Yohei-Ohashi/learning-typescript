f5a07068fbd70237acc5f4bc91c6b50f759d96ea
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 19:59:28 +0900

    17. 合計金額を表示しよう - 文字列連結

---
+++ js/strings.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("100円の商品を5個買った合計は" + (100 * 5) + "円です");
+console.log("1+1=" + (1 + 1));
+console.log(`100円の商品を5個買った合計は${100 * 5}円です`);
+//# sourceMappingURL=strings.js.map
