f223bc93effc56930d5ef3c3ae13fcb286fdefbf
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 23:09:38 +0900

    23. 今日の日付を確認しよう① - Dateオブジェクト

---
+++ date.ts
@@ -0,0 +1,4 @@
+const today: Date = new Date();
+const newyear: Date = new Date(2024, 0, 1);
+console.log(today.getDate());
+console.log(newyear.getDate());
---
+++ js/date.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const today = new Date();
+const newyear = new Date(2024, 0, 1);
+console.log(today.getDate());
+console.log(newyear.getDate());
+//# sourceMappingURL=date.js.map
