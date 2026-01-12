3db455d9e5fcbd4e2547612f55eb0f77dcf4eba6
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 16:43:13 +0900

    9. TypeScriptの設定をしよう

--- js/sum.js
+++ js/sum.js
@@ -1,8 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log(1 + 1);
 console.log(10 - 2);
 console.log(10 * 5);
 console.log(10 / 5);
 console.log(10 % 3);
-console.log(Math.pow(10, 2));
+console.log(10 ** 2);
 console.log((1 + 2) * 5);
-console.log(1.9 - 0.1);
+//# sourceMappingURL=sum.js.map
