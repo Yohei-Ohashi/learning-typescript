4da6d47031c740c5205a33ce927495fe1da293ab
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 17:19:11 +0900

    12. 変数を使おう

--- js/tax.js
+++ js/tax.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let tax = 1.1; // 消費税を10%とする
+const cnt = 1;
 console.log(500 * tax);
 console.log(200 * tax);
 tax = 1.08; // 消費税を8%にする
--- tax.ts
+++ tax.ts
@@ -1,4 +1,5 @@
 let tax: number = 1.1;  // 消費税を10%とする
+const cnt: number = 1;
 
 console.log(500 * tax);
 console.log(200 * tax);
