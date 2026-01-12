65725fe2a0058c8966fbdf25376ed9d2256874ad
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-12 23:51:19 +0900

    27. インクリメント・デクリメントについて理解しよう

--- for.ts
+++ for.ts
@@ -1,3 +1,16 @@
-for (let i: number = 1; i <= 100; i++) {
-  console.log(i);
-}
+// for (let i: number = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+let count: number = 1;
+count = count + 1;
+console.log(count);
+
+count += 1;
+count -= 1;
+count *= 2;
+count /= 3;
+console.log(count);
+
+count++;  // インクリメント
+count--;  // デクリメント
--- js/for.js
+++ js/for.js
@@ -1,6 +1,16 @@
 "use strict";
+// for (let i: number = 1; i <= 100; i++) {
+//   console.log(i);
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
+let count = 1;
+count = count + 1;
+console.log(count);
+count += 1;
+count -= 1;
+count *= 2;
+count /= 3;
+console.log(count);
+count++; // インクリメント
+count--; // デクリメント
 //# sourceMappingURL=for.js.map
