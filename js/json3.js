8f38d90ace860a528c5e43fe1b0145046aee81c0
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-19 00:32:05 +0900

    59. 型エイリアスで型情報を簡単に記述しよう

---
+++ js/json3.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const count = 1;
+const item = [
+    {
+        "name": "いちご",
+        "price": 150
+    }
+];
+//# sourceMappingURL=json3.js.map
---
+++ json3.ts
@@ -0,0 +1,15 @@
+type Item = {
+  name: string,
+  price?: number
+}
+type Num = number;
+
+const count: Num = 1;
+
+const item: Item[] = 
+[
+  {
+    "name": "いちご",
+    "price": 150
+  }
+];
