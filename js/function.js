e16783bcec656bee10285a8368568b3dabffba2e
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-18 18:05:54 +0900

    38. サイコロを振る関数を作ろう - 関数

---
+++ js/function.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getDice() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+function sayHello() {
+    console.log("こんにちは！");
+    return true;
+}
+sayHello();
+console.log(getDice());
+console.log(getDice());
+console.log(getDice());
+//# sourceMappingURL=function.js.map
